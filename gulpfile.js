6f732798a3b02309a5205807f79999b4a7b63577
L.P <parisot.laurent@gmail.com>
2017-06-05 15:55:14 +0200

    Add new example of angular scope

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('clean', function() {
 
 // minify CSS and uglify JS, need to check JS code before
 gulp.task('usemin', ['jshint'], function () {
-  return gulp.src('./app/contactus.html')
+  return gulp.src('./app/dishdetail.html')
     .pipe(usemin({
       js: [ngannotate(), uglify(), rev()],
       css:[minifycss(), rev()]
@@ -73,7 +73,7 @@ gulp.task('watch', ['default'], function () {
   browserSync.init({
     server: {
        baseDir: "dist",
-       index: "contactus.html"
+       index: "dishdetail.html"
     }
   });
 
