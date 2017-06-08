a43f0fbee1ba457a4714c259294f63bf510b1a07
L.P <parisot.laurent@gmail.com>
2017-06-08 12:39:11 +0200

    Single page application using angular routing capabilities.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,29 @@
 'use strict';
 
-angular.module('confusionApp', []);
+
+angular.module('confusionApp', ['ngRoute'])
+  // see https://stackoverflow.com/questions/41211875/angularjs-1-6-0-latest-now-routes-not-working
+  // https://github.com/angular/angular.js/pull/14202
+  .config(['$locationProvider', function($locationProvider) {
+    $locationProvider.hashPrefix('');
+  }])
+
+  .config(function($routeProvider) {
+    $routeProvider
+      // route for the contactus page
+      .when('/contactus', {
+        templateUrl : 'contactus.html',
+        controller  : 'ContactController'
+      })
+      // route for the menu page
+      .when('/menu', {
+        templateUrl : 'menu.html',
+        controller  : 'MenuController'
+      })
+      // route for the dish details page
+      .when('/menu/:id', {
+        templateUrl : 'dishdetail.html',
+        controller  : 'DishDetailController'
+      })
+      .otherwise('/contactus');
+  });
